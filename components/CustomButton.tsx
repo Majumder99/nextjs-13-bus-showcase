04339f41c67603d05f7663c186e223ae077b1a8d
Sourav Majumder <souravmajumderk99@gmail.com>
2023-08-15 23:26:37 +0600

    hero section

---
+++ components/CustomButton.tsx
@@ -0,0 +1,12 @@
+import { CustomButtonProps } from "@/types";
+import React from "react";
+
+const CustomButton = ({
+  title,
+  containerStyles,
+  handleClick,
+}: CustomButtonProps) => {
+  return <></>;
+};
+
+export default CustomButton;
